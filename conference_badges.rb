fddebc57d53ae4222c39f1005c8867a0e196939a
Tanuka Das <das.tanuka@yahoo.com>
2019-04-28 17:38:54 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,13 @@
 # Write your code here.
+def badge_maker(name)
+  puts "Hello, my name is #{name}."
+
+end
+
+def batch_badge_creator(attendees)
+  attendees.each do |name|
+    puts "badge_maker(name)"
+    #puts badge_maker"(#{name})"
+  end
+end
+#return "Hello, my name is #{name}."
