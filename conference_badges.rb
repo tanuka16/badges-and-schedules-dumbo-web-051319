28d9f101bb5bbcff6150b308ee3e0c04d6087713
Tanuka Das <das.tanuka@yahoo.com>
2019-04-28 17:50:45 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -5,7 +5,7 @@ def badge_maker(name)
 end
 
 def batch_badge_creator(attendees)
-  attendees.each do |name|
+  attendees.collect do |name|
     puts "badge_maker(name)"
     #puts badge_maker"(#{name})"
   end
