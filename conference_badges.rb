63c449ba00f3bc65b794d1b9e7b01006d76ba224
Tanuka Das <das.tanuka@yahoo.com>
2019-04-28 18:22:03 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -13,7 +13,7 @@ def batch_badge_creator(attendees)
 end
 
 def assign_rooms(attendees)
-  speakers.collect.each_with_index do |name, index|
+  attendees.collect.each_with_index do |name, index|
     "Hello, #{name}! You'll be assigned to room #{index+1}!"
   end
 end
