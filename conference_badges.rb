4dddcfa5e745e8138cb42f7e56948fc43a7219df
Tanuka Das <das.tanuka@yahoo.com>
2019-04-28 18:25:36 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -5,12 +5,25 @@ def badge_maker(name)
 end
 
 def batch_badge_creator(attendees)
-  attendees.each do |name|
-    puts "badge_maker(name)"
+  attendees.collect do |name|
+    badge_maker(name)
+    #puts badge_maker(name)
     #puts badge_maker"(#{name})"
   end
 end
 
-def assign_rooms
+def assign_rooms(attendees)
+  attendees.collect.each_with_index do |name, index|
+    "Hello, #{name}! You'll be assigned to room #{index+1}!"
+  end
+end
 
+
+def printer(attendees)
+  batch_badge_creator(attendees).each do |name|
+    puts name
+  end
+  assign_rooms(attendees).each do |name|
+    puts name
+  end
 end
